34a533980ab182bb649142aebe8b418f9953555f
lucassaporetti <lucassaporetti@gmail.com>
2020-06-05 19:31:22 -0300

    add page under progress

--- src/core/service/service_facade.py
+++ src/core/service/service_facade.py
@@ -1,59 +1,59 @@
-# from abc import ABC
-# from typing import Type
-#
-# from core.config.app_configs import AppConfigs
-# from src.core.enum.database_type import DatabaseType
-# from src.core.enum.model import Model
-# from src.core.enum.repository_type import RepositoryType
-# from src.core.service.car_service import CarService
-# from src.core.service.customer_service import CustomerService
-# from src.core.service.employee_service import EmployeeService
-# from src.core.service.rental_service import RentalService
-# from src.core.service.service import Service
-# from src.core.tools.commons import log_init
-#
-# LOG = log_init(AppConfigs.log_file())
-#
-#
-# class ServiceFacade(ABC):
-#     __cache = {}
-#     __services = {
-#         Model.CAR.name: CarService,
-#         Model.CUSTOMER.name: CustomerService,
-#         Model.EMPLOYEE.name: EmployeeService,
-#         Model.RENTAL.name: RentalService,
-#     }
-#
-#     @staticmethod
-#     def create_or_get(service_class: Type, repository_type: RepositoryType, database_type: DatabaseType):
-#         cache_key = service_class.__name__
-#         if cache_key in ServiceFacade.__cache:
-#             LOG.info('Retrieving service {}'.format(cache_key))
-#             return ServiceFacade.__cache[cache_key]
-#         else:
-#             LOG.info('Creating service {}'.format(cache_key))
-#             ServiceFacade.__cache[cache_key] = service_class(repository_type, database_type)
-#             return ServiceFacade.__cache[cache_key]
-#
-#     @staticmethod
-#     def get(repository_type: RepositoryType, database_type: DatabaseType, model: Model) -> Service:
-#         service_class = ServiceFacade.__services[model.name]
-#         service = ServiceFacade.create_or_get(service_class, repository_type, database_type)
-#         LOG.info('Retrieving service: {}'.format(service))
-#         return service
-#
-#     @staticmethod
-#     def get_car_service() -> Service:
-#         return ServiceFacade.get(AppConfigs.repository_type(), AppConfigs.database_type(), Model.CAR)
-#
-#     @staticmethod
-#     def get_customer_service() -> Service:
-#         return ServiceFacade.get(AppConfigs.repository_type(), AppConfigs.database_type(), Model.CUSTOMER)
-#
-#     @staticmethod
-#     def get_employee_service() -> Service:
-#         return ServiceFacade.get(AppConfigs.repository_type(), AppConfigs.database_type(), Model.EMPLOYEE)
-#
-#     @staticmethod
-#     def get_rental_service() -> Service:
-#         return ServiceFacade.get(AppConfigs.repository_type(), AppConfigs.database_type(), Model.RENTAL)
+from abc import ABC
+from typing import Type
+
+from core.config.app_configs import AppConfigs
+from src.core.enum.database_type import DatabaseType
+from src.core.enum.model import Model
+from src.core.enum.repository_type import RepositoryType
+from src.core.service.car_service import CarService
+from src.core.service.customer_service import CustomerService
+from src.core.service.employee_service import EmployeeService
+from src.core.service.rental_service import RentalService
+from src.core.service.service import Service
+from src.core.tools.commons import log_init
+
+LOG = log_init(AppConfigs.log_file())
+
+
+class ServiceFacade(ABC):
+    __cache = {}
+    __services = {
+        Model.CAR.name: CarService,
+        Model.CUSTOMER.name: CustomerService,
+        Model.EMPLOYEE.name: EmployeeService,
+        Model.RENTAL.name: RentalService,
+    }
+
+    @staticmethod
+    def create_or_get(service_class: Type, repository_type: RepositoryType, database_type: DatabaseType):
+        cache_key = service_class.__name__
+        if cache_key in ServiceFacade.__cache:
+            LOG.info('Retrieving service {}'.format(cache_key))
+            return ServiceFacade.__cache[cache_key]
+        else:
+            LOG.info('Creating service {}'.format(cache_key))
+            ServiceFacade.__cache[cache_key] = service_class(repository_type, database_type)
+            return ServiceFacade.__cache[cache_key]
+
+    @staticmethod
+    def get(repository_type: RepositoryType, database_type: DatabaseType, model: Model) -> Service:
+        service_class = ServiceFacade.__services[model.name]
+        service = ServiceFacade.create_or_get(service_class, repository_type, database_type)
+        LOG.info('Retrieving service: {}'.format(service))
+        return service
+
+    @staticmethod
+    def get_car_service() -> Service:
+        return ServiceFacade.get(AppConfigs.repository_type(), AppConfigs.database_type(), Model.CAR)
+
+    @staticmethod
+    def get_customer_service() -> Service:
+        return ServiceFacade.get(AppConfigs.repository_type(), AppConfigs.database_type(), Model.CUSTOMER)
+
+    @staticmethod
+    def get_employee_service() -> Service:
+        return ServiceFacade.get(AppConfigs.repository_type(), AppConfigs.database_type(), Model.EMPLOYEE)
+
+    @staticmethod
+    def get_rental_service() -> Service:
+        return ServiceFacade.get(AppConfigs.repository_type(), AppConfigs.database_type(), Model.RENTAL)
--- src/ui/qt/view/item_add_ui.py
+++ src/ui/qt/view/item_add_ui.py
@@ -1,117 +1,46 @@
-from PyQt5.QtWidgets import QDialog, QDialogButtonBox
-from core.enum.color import Color
-from core.enum.fuel import Fuel
-from core.eventbus.event_bus import EventBus
-from core.model.car import Car
-from core.service.service_facade import ServiceFacade
-from ui.qt.views.qt_view import QtView
+from PyQt5.QtWidgets import *
+
+from core.config.app_configs import AppConfigs
+from core.service.item_service import ItemService
+from src.ui.qt.view.qt_view import QtView
 
 
-class ItemEditView(QtView):
+class ItemAddUi(QtView):
     def __init__(self, window: QDialog, parent: QtView):
         super().__init__(window, parent)
-        self.car_service = ServiceFacade.get_car_service()
-        self.selected_car = None
-        self.addPage = self.qt.find_widget(self.window, QWidget, 'informationPage')
-        self.addDescriptionEdit = self.qt.find_text_edit('addDescriptionEdit')
-        self.addSpecialAttEdit = self.qt.find_text_edit('addSpecialAttEdit')
+        self.item_service = ItemService(AppConfigs.repository_type(), AppConfigs.database_type(), Model.CAR)
+        self.selected_item = None
+        self.addPage = self.qt.find_widget(self.window, QWidget, 'addPage')
         self.addCategoryBox = self.qt.find_combo_box('addCategoryBox')
-        self.addDroppedBox = self.qt.find_combo_box('addDroppedBox')
-        self.addFoundBox = self.qt.find_combo_box('addFoundBox')
-        self.addSpecialityBox = self.qt.find_combo_box('addSpecialityBox')
         self.addTypeBox = self.qt.find_combo_box('addTypeBox')
-        self.addGifAnimation = self.qt.find_label('addGifAnimation')
-        self.addGifSpecial = self.qt.find_label('addGifSpecial')
-        self.addItemImage = self.qt.find_label('addItemImage')
+        self.addDescriptionEdit = self.qt.find_text_edit('addDescriptionEdit')
+        self.addAttributes = self.qt.find_combo_box('addAttributesBox')
+        self.addConsumeMp = self.qt.find_spin_box('addConsumeMp')
+        self.addConsumeHt = self.qt.find_spin_box('addConsumeHt')
+        self.addStatHp = self.qt.find_spin_box('addStatHp')
+        self.addStatMp = self.qt.find_spin_box('addStatMp')
+        self.addStatHt = self.qt.find_spin_box('addStatHt')
+        self.addStatStr = self.qt.find_spin_box('addStatStr')
+        self.addStatAtt = self.qt.find_spin_box('addStatAtt')
+        self.addStatInt = self.qt.find_spin_box('addStatInt')
+        self.addStatCon = self.qt.find_spin_box('addStatCon')
+        self.addStatMaxHt = self.qt.find_spin_box('addStatMaxHt')
+        self.addStatMaxHp = self.qt.find_spin_box('addStatMaxHp')
+        self.addStatDef = self.qt.find_spin_box('addStatDef')
+        self.addStatLck = self.qt.find_spin_box('addStatLck')
+        self.addStatGold = self.qt.find_spin_box('addStatGold')
+        self.addSell = self.qt.find_double_spin_box('addSell')
+        self.addFoundBox = self.qt.find_combo_box('addFoundBox')
+        self.addDroppedBox = self.qt.find_combo_box('addFDroppedBox')
+        self.addEffectEdit = self.qt.find_text_edit('addEffectEdit')
         self.addNameEdit = self.qt.find_line_edit('addNameEdit')
+        self.addItemImage = self.qt.find_label('addItemImage')
+        self.addItemAnimation = self.qt.find_label('addItemAnimation')
+        self.addItemSpecialAnimation = self.qt.find_label('addItemSpecialAnimation')
         self.cancelButton = self.qt.find_push_button('cancelButton')
         self.resetButton = self.qt.find_push_button('resetButton')
         self.saveButton = self.qt.find_push_button('saveButton')
+        self.setup_ui()
         # self.animated_item_gif()
         # self.show_item_image()
-        self.setup_ui()
 
-
-
-
-            self.leCarName = self.qt.find_line_edit('leCarName')
-            self.leChassis = self.qt.find_line_edit('leChassis')
-            self.cmbColor = self.qt.find_combo_box('cmbColor')
-            self.spbDoors = self.qt.find_spin_box('spbDoors')
-            self.cmbFuel = self.qt.find_combo_box('cmbFuel')
-            self.lePlate = self.qt.find_line_edit('lePlate')
-            self.spbPrice = self.qt.find_double_spin_box('spbPrice')
-            self.bbAddCar = self.qt.find_button_box('bbAddCar')
-            self.setup_ui()
-
-        def setup_ui(self):
-            EventBus.get('car-selection-bus').subscribe('rowSelected', self.car_selected)
-            self.bbAddCar.accepted.connect(self.on_save)
-            self.bbAddCar.rejected.connect(self.on_cancel)
-            self.bbAddCar.button(QDialogButtonBox.Reset).clicked.connect(self.on_reset)
-
-        def car_selected(self, args):
-            car = args['selected_item']
-            self.log.info('Car selected for update: {}'.format(car))
-            self.selected_car = car
-            self.leCarName.setText(car.name)
-            self.leChassis.setText(car.chassis)
-            self.cmbColor.setCurrentText(car.color.name)
-            self.spbDoors.setValue(car.doors)
-            self.cmbFuel.setCurrentText(car.fuel.name)
-            self.lePlate.setText(car.plate)
-            self.spbPrice.setValue(car.price)
-
-        def on_reset(self):
-            self.log.info('Car form reset')
-            self.selected_car = None
-            self.leCarName.setText(None)
-            self.leChassis.setText(None)
-            self.cmbColor.setCurrentIndex(0)
-            self.spbDoors.setValue(3)
-            self.cmbFuel.setCurrentIndex(0)
-            self.lePlate.setText(None)
-            self.spbPrice.setValue(0.00)
-            self.window.repaint()
-
-        def on_save(self):
-            self.selected_car = self.selected_car if self.selected_car else Car()
-            self.selected_car.name = self.leCarName.text()
-            self.selected_car.chassis = self.leChassis.text()
-            self.selected_car.color = Color[self.cmbColor.currentText()]
-            self.selected_car.doors = self.spbDoors.value()
-            self.selected_car.fuel = Fuel[self.cmbFuel.currentText()]
-            self.selected_car.plate = self.lePlate.text()
-            self.selected_car.price = self.spbPrice.value()
-            self.selected_car.available = self.selected_car.available.value
-            self.car_service.save(self.selected_car)
-            self.parent.car_search.btn_search_car_clicked()
-            self.parent.car_search.stackedPanelCars.setCurrentIndex(0)
-            self.on_reset()
-            self.log.info('Car saved: {}'.format(self.selected_car))
-
-        def on_cancel(self):
-            self.on_reset()
-            self.parent.car_search.stackedPanelCars.setCurrentIndex(0)
-
-    # def show_item_image(self):
-    #     item_image = '/home/lucassaporetti/GIT-Repository/' \
-    #                  'castlevania_inventory_system/src/' \
-    #                  'resources/images/items/alucard_sword_icon.png'
-    #     qimage = QtGui.QImage(item_image)
-    #     pixmap = QtGui.QPixmap.fromImage(qimage)
-    #     pixmap_image = QtGui.QPixmap(pixmap)
-    #     self.itemImage.setPixmap(pixmap_image)
-    #     self.itemImage.show()
-
-    # def animated_item_gif(self):
-    #     movie = QtGui.QMovie("/home/lucassaporetti/GIT-Repository/"
-    #                          "castlevania_inventory_system/src/resources/"
-    #                          "images/items/alucard_sword_animation.gif")
-    #     self.itemGifAnimation.setMovie(movie)
-    #     movie.start()
-    #     movie2 = QtGui.QMovie("/home/lucassaporetti/GIT-Repository/"
-    #                           "castlevania_inventory_system/src/resources/"
-    #                           "images/items/alucard_sword_special.gif")
-    #     self.itemGifSpecial.setMovie(movie2)
-    #     movie2.start()
