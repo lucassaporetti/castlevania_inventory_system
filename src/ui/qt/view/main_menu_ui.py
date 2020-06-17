662cdcab0bd38759691921f5d079018bc9f9df06
LucasSaporetti <lucassaporetti@gmail.com>
2020-06-17 17:19:43 -0300

    images right

--- src/ui/qt/view/main_menu_ui.py
+++ src/ui/qt/view/main_menu_ui.py
@@ -1,10 +1,9 @@
-from PyQt5 import uic, QtGui
+from PyQt5 import uic
 from PyQt5.QtWidgets import *
 from src.ui.qt.view.qt_view import QtView
 from ui.qt.view.item_edit_ui import ItemEditUi
 from src.ui.qt.view.item_add_ui import ItemAddUi
 from src.ui.qt.view.item_information_ui import ItemInformationUi
-from src.resources.resources_properties.image_paths import ImagePaths
 
 
 class MainMenuUi(QtView):
@@ -24,8 +23,6 @@ class MainMenuUi(QtView):
         self.searchLine = self.qt.find_line_edit('searchLine')
         self.stackedMain = self.qt.find_stacked_widget('stackedMain')
         self.mainPage = self.qt.find_widget(self.window, QWidget, 'mainPage')
-        self.logoImage = self.qt.find_label('logoImage')
-        self.show_logo_image()
         self.setup_ui()
 
     def setup_ui(self):
@@ -43,12 +40,3 @@ class MainMenuUi(QtView):
     def add_button_clicked(self):
         self.itemInformationUi.categoryBox.setCurrentIndex(8)
         self.stackedMain.setCurrentIndex(2)
-
-    def show_logo_image(self):
-        logo_image = ImagePaths(7).get_image()
-        q_image = QtGui.QImage(logo_image)
-        q_pixmap = QtGui.QPixmap.fromImage(q_image)
-        q_pixmap_image = QtGui.QPixmap(q_pixmap)
-        q_pixmap_image_sized = q_pixmap_image.scaled(307, 164)
-        self.logoImage.setPixmap(q_pixmap_image_sized)
-        self.logoImage.show()
